613bca3244de154e6eecb895b18951008f6b870f
mabiane <mabiane.polniak@gmail.com>
2021-10-13 18:50:28 -0300

    requisito 1 - concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => {
+  const especimeUnico = species.filter((animal) => animal.id === ids[0]);
+  const especimes = species.filter((animal) => animal.id === ids[0] || animal.id === ids[1]);
+
+  if (ids.length === 1) {
+    return especimeUnico;
+  }
+  if (ids.length > 1) {
+    return especimes;
+  }
+  return [];
+};
 
 module.exports = getSpeciesByIds;
