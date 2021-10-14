e7d866dadf80f63dabc2aa811db87e6cf83323d2
mabiane <mabiane.polniak@gmail.com>
2021-10-14 10:59:19 -0300

    requisito 2 - concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,15 +1,16 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => species.filter((specie) => specie.name === animal)
-  .every((specie) => { 
-    if (specie.residents.age > age) {;
-      return true;
-    } else {
-      return false
-    }
-  });
+function getAnimalsOlderThan(animal, age) {
+  let testAge;
 
-console.log(getAnimalsOlderThan('otters', 7));
+  species.filter((specie) => specie.name === animal)
+    .forEach((prop) => {
+      const selecionado = prop.residents;
+      testAge = selecionado.every((res) => res.age > age);
+    });
+
+  return testAge;
+}
 
 module.exports = getAnimalsOlderThan;
