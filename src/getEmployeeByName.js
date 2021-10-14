20e5aa162bc1315e47664774221bf5eba1a70e3f
mabiane <mabiane.polniak@gmail.com>
2021-10-14 15:35:39 -0300

    requisito 3 - concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+/**
+   *  testar se a função esta vazia e retornar obj {}
+   *
+   * percorrer funcionarios nome testando parametro
+   *
+   * percorrer funcionarios sobrenome testando parametro
+   */
+
+const getEmployeeByName = (employeeName) => {
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const fullName = employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+
+  return fullName;
+};
 
 module.exports = getEmployeeByName;
