16eef6a7d24e0cb03b33c35f4eba510b5dd8d109
mabiane <mabiane.polniak@gmail.com>
2021-10-14 17:42:17 -0300

    requisito 4 - em andamento - funçao isManager criada

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+/**
+ * is manager - verifica se o colaborador é um gerente ou não
+ * filtrar pelo id retornando booleano
+ *
+ * se responsibleFor > 3
+ * $$ manager === [stephanieId] ou []
+ */
+
 function isManager(id) {
-  // seu código aqui
+  const findId = employees.filter((person) =>
+    `${person.firstName.toLowerCase()}Id` === id);
+
+  const findManager = findId.some((item) =>
+    item.managers.length === 0 || item.responsibleFor.length > 3);
+
+  return findManager;
 }
+
+console.log(isManager('olaId'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
--- test/getRelatedEmployees.test.js
+++ test/getRelatedEmployees.test.js
@@ -8,7 +8,7 @@ const managers = [stephanieId, olaId, burlId];
 
 describe('4 - Crie a gestão dos funcionários de acordo com o respectivo gerente', () => {
   it('retorna true se o id passado é de um gerente', () => {
-    const expected = true;
+    const expected = true;  
     
     managers.forEach((managerId) => {
       const actual = isManager(managerId);
