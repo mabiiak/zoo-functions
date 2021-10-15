dfe74bf43c53faca3ea82aed6557838cd7b766e3
mabiane <mabiane.polniak@gmail.com>
2021-10-15 16:35:01 -0300

    requisito 9 - em andamento

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,8 @@
 const { employees } = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const data = require('../data/zoo_data');
+
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 // const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
 
 /**
@@ -15,13 +16,12 @@ OK
 *filter 2:
   procurar em species o animalID - desconstrução?
 *retorno: array completo com todos os animais daquela especie
-OK
-
-*sort:
-  ordenamos o array criado com filter2,
-  do mais velho para o mais novo com base no residents.age
-  filter2 retornou um array de obejtos, sort não está acessando :/
-*
+  o filter um retorna um array, logo o filter dois trabalha com a posição[0]
+    *sort dentro do filtro, pois fora ele não consegue acessar
+      ordenamos o array criado com filter2
+      do mais velho para o mais novo com base no residents.age
+      filter2 retornou um array de obejtos, sort não está acessando :/
+OKOK
 
 *retono final?
 *map:
@@ -33,18 +33,16 @@ OK
 const employeeResponsibleFor = (id) => employees.filter((person) => person.id === id)
   .map(({ responsibleFor }) => responsibleFor[0]);
 
-console.log(employeeResponsibleFor(burlId)); // retorna ID animal [0]
-
-// filter 2
-const firstAnimal = (idAnimal) => species.filter((animal) => animal.id === idAnimal)
-  .map(({ residents }) => residents);
-
-console.log(firstAnimal('0938aa23-f153-4937-9f88-4858b24d6bce'));
-// retorna lista daquela espécie, porém como vamos passar o filter1 como função?
-console.log(firstAnimal(employeeResponsibleFor(burlId)));
+const firstAnimal = (idAnimal) => species.filter((animal) => animal.id === idAnimal[0])
+  .map(({ residents }) => residents.sort((a, b) => b.age - a.age))
+  .map((animal) => `${animal[0].name} , ${animal[0].sex} , ${animal[0].age}`);
+  // .map((animal) => [animal.name, animal.sex , animal.age]);
+  // o array contem 3 item- length 2| atualmente ele é um só
 
 function getOldestFromFirstSpecies(id) {
-  // adicione
+  return firstAnimal(employeeResponsibleFor(id));
 }
+
+console.log(getOldestFromFirstSpecies(olaId));
 
 module.exports = getOldestFromFirstSpecies;
