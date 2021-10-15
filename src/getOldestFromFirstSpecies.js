f74610070f6bfafbc09dff5d5a9469276cd7e2ec
mabiane <mabiane.polniak@gmail.com>
2021-10-15 19:31:16 -0300

    requisito 9 - concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,32 +2,15 @@ const { employees } = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-// const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
-
 /**
 *filter 1:
-  dado um ID funcionario
-  e employees.id === parametroID
-  retorno: array com iformações do funcionario
-  map para buscar o ID do primeiro animal
-OK
+  dado um ID funcionario que employees.id === parametroID
+  retorno: array com primeiro animal
 
 *filter 2:
-  procurar em species o animalID - desconstrução?
-*retorno: array completo com todos os animais daquela especie
-  o filter um retorna um array, logo o filter dois trabalha com a posição[0]
-    *sort dentro do filtro, pois fora ele não consegue acessar
-      ordenamos o array criado com filter2
-      do mais velho para o mais novo com base no residents.age
-      filter2 retornou um array de obejtos, sort não está acessando :/
-OKOK
-
-*retono final?
-*map:
-  percorrei filter2 já ordenado,
-  criar array com: [name, sexo, age]
-  o item [0] do filter2
+  procurar em species o animalID
+  o filter um retorna um array completo do animal, ordenamos os residentes por idade
+  depois puxamos o valor do item na posição 0
 */
 
 const employeeResponsibleFor = (id) => employees.filter((person) => person.id === id)
@@ -35,14 +18,10 @@ const employeeResponsibleFor = (id) => employees.filter((person) => person.id ==
 
 const firstAnimal = (idAnimal) => species.filter((animal) => animal.id === idAnimal[0])
   .map(({ residents }) => residents.sort((a, b) => b.age - a.age))
-  .map((animal) => `${animal[0].name} , ${animal[0].sex} , ${animal[0].age}`);
-  // .map((animal) => [animal.name, animal.sex , animal.age]);
-  // o array contem 3 item- length 2| atualmente ele é um só
+  .map((item) => Object.values(item[0]));
 
 function getOldestFromFirstSpecies(id) {
-  return firstAnimal(employeeResponsibleFor(id));
+  return firstAnimal(employeeResponsibleFor(id))[0];
 }
-
-console.log(getOldestFromFirstSpecies(olaId));
 
 module.exports = getOldestFromFirstSpecies;
