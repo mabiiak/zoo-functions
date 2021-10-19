6a551227c3b89482c6f583987bf58d8eb39ace2e
mabiane <mabiane.polniak@gmail.com>
2021-10-19 14:37:00 -0300

    requisito 6 - countEntrants lint corrigido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -12,23 +12,15 @@ function countEntrants(entrants) {
   let adult = 0;
   let senior = 0;
 
-  entrants.map((pessoa) => {
-    if (pessoa.age < 18) {
-      child += 1;
-    }
-    if (pessoa.age >= 18
-      && pessoa.age < 50) {
-      adult += 1;
-    }
-    if (pessoa.age >= 50) {
-      senior += 1;
-    }
-    
+  entrants.forEach((pessoa) => {
+    if (pessoa.age < 18) child += 1;
+    if (pessoa.age >= 18 && pessoa.age < 50) adult += 1;
+    if (pessoa.age >= 50) senior += 1;
   });
   return { child, adult, senior };
 }
 
-// console.log(countEntrants([{ age: 5 },{  age: 5 },{  age: 5 },{  age: 18 },{  age: 18 },{age: 50 }])) // teste contador de idade
+console.log(countEntrants([{age: 5}, {age: 5}, {age: 5}, {age: 18}, {age: 18}, {age: 50}])) // teste contador de idade
 
 function calculateEntry(entrants) {
   // seu código aqui
